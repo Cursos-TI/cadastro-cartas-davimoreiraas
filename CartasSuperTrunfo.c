a0b470312cedd2312c6202786124c63d64482bf4
Davi Moreira <davi.alves0211@gmail.com>
2026-02-20 13:45:39 +0000

    Entrada de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,11 +7,58 @@
 int main() {
  printf("Desafio Xadrez!\n");
  printf("Novo commmit\n");
-  // Área para definição das variáveis para armazenar as propriedades das cidades
 
-  // Área para entrada de dados
+// Área para definição das variáveis para armazenar as propriedades das cidades
 
-  // Área para exibição dos dados da cidade
+// Variáveis para Carta 1
+
+ char estado1;
+ char codigo1[4];
+ char nomeCidade1[50];
+ int populacao1;
+ float area1, pib1;
+ int pontosTuristicos1;
+
+// Variáveis para Carta 2
+
+ char estado2;
+ char codigo2[4];
+ char nomeCidade2[50];
+ int populacao2;
+ float area2, pib2;
+ int pontosTuristicos2;
+
+// Área para entrada de dados
+
+//Entrada de dados Carta 1 
+
+ printf("Carta 1 \n");
+
+ printf("Estado (A-H):\n");
+ scanf("%c", &estado1);
+
+ printf("Código da Carta:\n");
+ scanf("%s", &codigo1);
+
+ printf("Nome da Cidade:\n");
+ scanf("%s", &nomeCidade1);
+
+ printf("População:\n");
+ sacnf("%d", &populacao1);
+
+ prinf("Área:\n");
+ scanf("%f", area1);
+
+ prinft("PIB:\n");
+ scanf("%f", pib1);
+
+ prinft("Números de Pontos Turísticos:\n");
+ scanf("%d", pontosTuristicos1);
+
+// Área para exibição dos dados da cidade
+
+ prinft("Carta 1:\n");
+ printf("Estado: %c\n", estado1);
 
 return 0;
 } 
